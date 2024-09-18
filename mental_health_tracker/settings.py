649c63ef1dab3559410dc7c76787cf278e271ff0
Simforianus.Jonathan <jonathanharefa9@gmail.com>
2024-09-18 15:49:34 +0700

    finish tutorial 3 by making login page and user page and connecting user with object MoodEntry in model

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7$6pw=p9$jxy4fh1-5jiz273_!+yv^t1r#htyc9+pw98bs!z-a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "simforianus-jonathan-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
